04e0766c8a3c8ec7d793a0559921a86e0b35cbb7
ohoraming <kminsmile@gmail.com>
2022-10-07 17:15:37 +0900

    기초 세팅 완료

---
+++ src/main/java/com/websocket/wsstudy/WsStudyApplication.java
@@ -0,0 +1,13 @@
+package com.websocket.wsstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/websocket/wsstudy/WsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocket.wsstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
